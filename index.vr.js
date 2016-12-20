1c13e03f30101ad9efda08016038d43e45a8f3c1
Muhammad Kamran <smkamranqadri@yahoo.com>
2016-12-20 19:55:06 +0500

    Inital Commit with basic app

---
+++ index.vr.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  StyleSheet,
+  Pano,
+  Text,
+  View,
+} from 'react-vr';
+
+class ReactVrTodo extends React.Component {
+  render() {
+    return (
+      <View>
+        <Pano source={asset('old-boys-school.jpg')}/>
+        <Text
+          style={{
+            backgroundColor:'blue',
+            padding: 0.02,
+            textAlign:'center',
+            textAlignVertical:'center',
+            fontSize: 0.8,
+            layoutOrigin: [0.5, 0.5],
+            transform: [{translate: [-10, 0, 0]}, , {rotateY : 90}],
+          }}>
+          hello
+        </Text>
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('react-vr-todo', () => ReactVrTodo);
